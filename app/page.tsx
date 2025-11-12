3c6c9d656c215d5497fed53d15818e69ef4580b9
FSA-22 <simeonstat@gmail.com>
2025-11-12 17:04:17 +0000

    Update gobal.css for custom coor and class

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import PrimaryButton from '@/components/PrimaryButton';
 
 const Home = () => {
   return (
-    <main className="flex justify-center items-center bg-linear-to-r from-purple-400 via-pink-500 to-cyan-100 min-h-screen">
+    <main className="flex-center bg-linear-to-r from-secondary via-pink-500 to-primary min-h-screen">
       <PrimaryButton />
     </main>
   );
