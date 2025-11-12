8a7708e72c4f57b74f6bc96d1f545008108f0ec3
FSA-22 <simeonstat@gmail.com>
2025-11-12 17:42:15 +0000

    Update css custom cass add constants and add amazona logo

---
+++ constants/index.tsx
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'MyApp';
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Pay less, live more';
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'The best e-commerce website in the world, built with Nextjs, and MongoDB - myapp-clone';
